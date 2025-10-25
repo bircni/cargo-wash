e37099a63dcdb43ea53b17882a526e178c8f5853
Nicolas <bircni@icloud.com>
2025-10-25 15:46:58 +0200

    feat: use condensed layout to improve table formatting in stats display

--- src/commands/stats.rs
+++ src/commands/stats.rs
@@ -1,4 +1,4 @@
-use comfy_table::Table;
+use comfy_table::{Table, presets::UTF8_FULL_CONDENSED};
 
 use crate::{
     commands::total_size_of_projects,
@@ -10,6 +10,7 @@ pub fn show(projects: &[Project]) {
     let mut sorted_projects: Vec<Project> = projects.to_vec();
     sorted_projects.sort_by(|a, b| a.name.to_lowercase().cmp(&b.name.to_lowercase()));
     let mut table = Table::new();
+    table.load_preset(UTF8_FULL_CONDENSED);
     table.set_header(vec!["Project", "Size", "Path"]);
 
     for project in sorted_projects {
